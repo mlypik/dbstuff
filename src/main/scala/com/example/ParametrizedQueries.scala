456474f7ed2fd5b7764c90c17ef1233d875659ed
emaclyp <maciej.lypik@ericsson.com>
2018-09-24 12:04:18 +0200

    adds doobie fragment example and statement logging

--- src/main/scala/com/example/ParametrizedQueries.scala
+++ src/main/scala/com/example/ParametrizedQueries.scala
@@ -3,6 +3,7 @@ package com.example
 import doobie._
 import doobie.implicits._
 import cats._
+import cats.data.NonEmptyList
 import cats.effect._
 import cats.implicits._
 
@@ -29,13 +30,41 @@ object ParametrizedQueries extends App {
     .quick
     .unsafeRunSync()
 
+
   def biggerThan(minPop: Int): doobie.Query0[Country] = sql"""
     select code, name, population, gnp
     from country
     where population > $minPop
     """.query[Country]
 
+  println("single param:")
   biggerThan(150000000).quick.unsafeRunSync()
 
+  def populationIn(range: Range) = sql"""
+    select code, name, population, gnp
+    from country
+    where population > ${range.min}
+    and population < ${range.max}
+    """.query[Country]
+
+  println("multiple params:")
+  populationIn(10000000 to 300000000).quick.unsafeRunSync()
+
+  def populationInCodes(range: Range, codes: NonEmptyList[String]) = {
+    val q = fr"""
+      select code, name, population, gnp
+      from country
+      where population > ${range.min}
+      and population < ${range.max}
+      and """ ++ Fragments.in(fr"code", codes)
+    q.queryWithLogHandler[Country](LogHandler.jdkLogHandler)
+  }
+
+  println("multiple params fragment:")
+  populationInCodes(10000000 to 300000000, NonEmptyList.of("USA", "BRA", "PAK", "GBR"))
+    .list
+    .transact(xa)
+    .unsafeRunSync()
+    .foreach(println)
 
 }
